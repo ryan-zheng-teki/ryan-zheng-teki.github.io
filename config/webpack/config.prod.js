d6b56e12a5df59fe1ffaa1d62c2df760cdf2b911
Ryan Zheng <nogrethumphrey@gmail.com>
2020-05-07 19:38:42 +0200

    use https for API_URI

--- config/webpack/config.prod.js
+++ config/webpack/config.prod.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 module.exports = ({ sourceDir, distDir }) => ({
     plugins: [
         new webpack.EnvironmentPlugin({
-            API_URI: "http://intelligencealley.com",
+            API_URI: "https://intelligencealley.com",
             })
     ]
 });
