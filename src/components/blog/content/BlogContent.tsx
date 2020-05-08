24af8a13835864f6a9bac9e5390fb36cc04882ee
nogrethumphrey@gmail.com <nogrethumphrey@gmail.com>
2020-05-08 08:39:00 +0000

    change the middle div classname to blog__content

--- src/components/blog/content/BlogContent.tsx
+++ src/components/blog/content/BlogContent.tsx
@@ -13,37 +13,39 @@ interface BlogContentProps {
 
 export const BlogContent:React.FC<BlogContentProps> = ({ pagedBlogs }) => {
   return (
-    <CurrentCategoryContext.Consumer>
-      {
-          ({ currentCategory }) => {
-            if(currentCategory === undefined || currentCategory === null) {
-              if(pagedBlogs.edges.length > 0) {
-                const blogsElement =  pagedBlogs.edges.map((blog) => {
-                  return <p key={blog.node.id}>{blog.node.content}</p>;
-                });
-                return <>{blogsElement}</>;
-              }
-              return <p>The author is too lazy, he does not have any blogs currrently</p>;
-            } 
-            return (
-              <BlogsForCategoryQuery
-                variables={{ categoryId: currentCategory }}
-              > 
-                {
-                        ({ data }) => {
-                          if(data && data.blogsForCategory && data.blogsForCategory.length > 0) {
-                            const blogs =  data && data.blogsForCategory.map((blog) => {
-                              return <p key={blog.id}>{blog.content}</p>;
-                            });
-                            return <>{blogs}</>;
-                          } 
-                          return <p>The author has not created any blogs</p>;
+    <div className="blog__content">
+      <CurrentCategoryContext.Consumer>
+        {
+            ({ currentCategory }) => {
+              if(currentCategory === undefined || currentCategory === null) {
+                if(pagedBlogs.edges.length > 0) {
+                  const blogsElement =  pagedBlogs.edges.map((blog) => {
+                    return <p key={blog.node.id}>{blog.node.content}</p>;
+                  });
+                  return <>{blogsElement}</>;
+                }
+                return <p>The author is too lazy, he does not have any blogs currrently</p>;
+              } 
+              return (
+                <BlogsForCategoryQuery
+                  variables={{ categoryId: currentCategory }}
+                > 
+                  {
+                          ({ data }) => {
+                            if(data && data.blogsForCategory && data.blogsForCategory.length > 0) {
+                              const blogs =  data && data.blogsForCategory.map((blog) => {
+                                return <p key={blog.id}>{blog.content}</p>;
+                              });
+                              return <>{blogs}</>;
+                            } 
+                            return <p>The author has not created any blogs</p>;
+                          }
                         }
-                      }
-              </BlogsForCategoryQuery>
-            );
-          }
-      }
-    </CurrentCategoryContext.Consumer>
+                </BlogsForCategoryQuery>
+              );
+            }
+        }
+      </CurrentCategoryContext.Consumer>
+    </div>
   );
 };
