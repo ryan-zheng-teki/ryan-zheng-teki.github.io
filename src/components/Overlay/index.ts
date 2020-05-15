07568ef62629ce28c64f4e9c5e4fbaaa39b73005
nogrethumphrey@gmail.com <nogrethumphrey@gmail.com>
2020-05-15 08:26:00 +0000

    reformat Overlay index file

--- src/components/Overlay/index.ts
+++ src/components/Overlay/index.ts
@@ -1,6 +1,19 @@
-import { InnerOverlayContextInterface, OverlayContext, OverlayContextInterface, OverlayTheme, OverlayType, ShowOverlayType } from './context';
+import {
+  InnerOverlayContextInterface,
+  OverlayContext,
+  OverlayContextInterface,
+  OverlayTheme,
+  OverlayType,
+  ShowOverlayType,
+} from './context';
 
 export { default as Overlay } from './Overlay';
 export { default as OverlayProvider } from './provider';
-export { InnerOverlayContextInterface, OverlayContext, OverlayContextInterface, OverlayTheme, OverlayType, ShowOverlayType, };
-
+export {
+  InnerOverlayContextInterface,
+  OverlayContext,
+  OverlayContextInterface,
+  OverlayTheme,
+  OverlayType,
+  ShowOverlayType,
+};
